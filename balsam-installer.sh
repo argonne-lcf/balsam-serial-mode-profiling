ac93f4576de2b60b2a92570ce53721b6e33b7873
msalim <msalim@anl.gov>
2020-08-19 14:05:55 -0500

    add balsam install script

---
+++ balsam-installer.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e 
+
+if [ $# -ne 1 ];
+then 
+    echo "Please provide path to virtual env that you wish to create"
+    exit 1
+fi
+
+env_path=$1
+tmp_dir=/tmp/$USER/balsam-install
+
+module swap PrgEnv-intel PrgEnv-gnu
+module load cray-python
+export CC=cc
+export CXX=CC
+export FC=ftn
+export MPICC=cc
+export MPICXX=CC
+
+python -m venv $env_path
+source $env_path/bin/activate
+
+mkdir -p $tmp_dir
+cd $tmp_dir
+
+git clone https://github.com/mpi4py/mpi4py.git
+cd mpi4py
+pip install .
+cd -
+
+git clone https://github.com/balsam-alcf/balsam 
+cd balsam
+git checkout serial-mode-perf
+pip install .
+
+echo "Balsam install done."
+echo "Use 'module load postgresql' to set up Postgres"
+echo "If in doubt, delete ~/.balsam/settings.json to refresh Balsam settings"
